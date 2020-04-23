c200256275a68c12f7122aebcf0c28cf46024377
gusevroman <949397@mail.ru>
2020-04-23 23:09:08 +0300

    fix: write user in store when logging

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,10 +1,8 @@
-/* eslint-disable no-useless-catch */
 import firebase from 'firebase/app';
 
 export default {
   actions: {
-    // eslint-disable-next-line no-unused-vars
-    async login({ dispatch, commit }, { email, password }) {
+    async login({ commit }, { email, password }) {
       try {
         await firebase.auth().signInWithEmailAndPassword(email, password);
       } catch (error) {
@@ -29,8 +27,9 @@ export default {
       const user = firebase.auth().currentUser;
       return user ? user.uid : null;
     },
-    async logout() {
+    async logout({ commit }) {
       await firebase.auth().signOut();
+      commit('clearInfo');
     },
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import auth from './auth';
+import info from './info';
 
 Vue.use(Vuex);
 
@@ -21,5 +22,6 @@ export default new Vuex.Store({
   },
   modules: {
     auth,
+    info,
   },
 });
