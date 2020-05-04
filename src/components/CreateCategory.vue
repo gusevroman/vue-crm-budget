73b415d1f567e66d9f0256635c780e740451136b
gusevroman <949397@mail.ru>
2020-05-04 23:43:44 +0300

    fix min length title

--- src/components/CreateCategory.vue
+++ src/components/CreateCategory.vue
@@ -8,14 +8,19 @@
             type="text"
             maxlength="30"
             minlength="3"
-            show-word-limit="true"
+            show-word-limit
             prefix-icon="el-icon-edit"
             placeholder="New category"
             autofocus="true"
           ></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="success" icon="el-icon-plus" circle @click="submitHandler"></el-button>
+          <el-button
+            type="success"
+            icon="el-icon-plus"
+            circle
+            @click.prevent="submitHandler"
+          ></el-button>
         </el-form-item>
       </el-form>
     </template>
@@ -31,17 +36,19 @@ export default {
   },
   methods: {
     async submitHandler() {
-      console.log(this.title);
-      try {
-        const category = await this.$store.dispatch('createCategory', {
-          title: this.title,
-        });
-        this.title = '';
-        this.$message({ message: 'Category created', type: 'success' });
-        this.$emit('created', category);
-      } catch (error) {
-        this.$message({ message: 'Anything wrong, please repeat', type: 'error' });
-        console.log(error);
+      if (this.title.length <= 2) {
+        this.$message({ message: 'Please, repeat the name of the category', type: 'error' });
+      } else {
+        try {
+          const category = await this.$store.dispatch('createCategory', {
+            title: this.title,
+          });
+          this.title = '';
+          this.$message({ message: 'Category created', type: 'success' });
+          this.$emit('created', category);
+        } catch (error) {
+          this.$message({ message: 'Anything wrong, please repeat', type: 'error' });
+        }
       }
     },
   },
