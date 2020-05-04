6ea40fdd25e9a56a1ec42ea1e929b37ce183d293
gusevroman <949397@mail.ru>
2020-05-05 01:52:23 +0300

    feat: update categories

--- src/components/EditCategory.vue
+++ src/components/EditCategory.vue
@@ -2,35 +2,24 @@
   <div>
     <template>
       <div>
-        <h4>Edit</h4>
+        <h4>Edit category</h4>
       </div>
-      <el-form ref="formEditCategories" :model="formEditCategories" label-width="5rem">
-        <el-form-item label="Select">
-          <el-select v-model="value" placeholder="Select">
-            <el-option-group v-for="group in options" :key="group.label" :label="group.label">
-              <el-option
-                v-for="item in group.options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              >
-              </el-option>
-            </el-option-group>
+      <el-form>
+        <el-form-item label="Edit category">
+          <el-select ref="select" v-model="current">
+            <el-option v-for="c of categories" :key="c.id" :value="c.id">{{ c.title }}</el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="Name">
-          <el-input v-model="formEditCategories.name"></el-input>
-        </el-form-item>
-        <el-form-item label="Min">
-          <el-input v-model="formEditCategories.min"></el-input>
-        </el-form-item>
-        <el-form-item>
+
+        <el-form-item label="New name the category">
+          <el-input id="name" v-model="title" type="text" prefix-icon="el-icon-edit"></el-input>
+
           <el-button
             type="warning"
             icon="el-icon-edit"
             mini
             circle
-            @click="editCategory"
+            @click.prevent="updateCategory"
           ></el-button>
         </el-form-item>
       </el-form>
@@ -40,60 +29,52 @@
 
 <script>
 export default {
-  data() {
-    return {
-      formCategories: {
-        title: '',
-      },
-      formEditCategories: {
-        title: '',
-      },
-      options: [
-        {
-          label: 'Popular cities',
-          options: [
-            {
-              value: 'Shanghai',
-              label: 'Shanghai',
-            },
-            {
-              value: 'Beijing',
-              label: 'Beijing',
-            },
-          ],
-        },
-        {
-          label: 'City name',
-          options: [
-            {
-              value: 'Chengdu',
-              label: 'Chengdu',
-            },
-            {
-              value: 'Shenzhen',
-              label: 'Shenzhen',
-            },
-          ],
-        },
-      ],
-      value: '',
-    };
+  props: {
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: () => ({
+    select: null,
+    title: '',
+    current: null,
+  }),
+  watch: {
+    current(catId) {
+      const { title } = this.categories.find((c) => c.id === catId);
+      this.title = title;
+    },
+  },
+  created() {
+    const { title } = this.categories[0];
+    this.current = title;
+    this.title = title;
+  },
+  mounted() {
+    // this.select = M.FormSelect.init(this.$refs.select);
+    // M.updateTextFields();
+  },
+  destroyed() {
+    if (this.select && this.select.destroy) {
+      this.select.destroy();
+    }
   },
   methods: {
-    async createCategory() {
-      try {
-        const category = await this.$store.dispatch('createCategory', {
-          title: this.title,
-        });
-        this.title = '';
-        this.$message({ message: 'Category created', type: 'success' });
-        this.$emit('created', category);
-      } catch (error) {
-        this.$message({ message: 'Anything wrong, please repeat', type: 'error' });
-        console.log(error);
+    async updateCategory() {
+      if (this.title.length < 3) {
+        this.$message({ message: 'Please, repeat the name of the category', type: 'error' });
+      } else {
+        try {
+          const categoryData = { id: this.current, title: this.title };
+          await this.$store.dispatch('updateCategory', categoryData);
+          this.$message({ message: 'Category updated successfully', type: 'success' });
+          this.$emit('updated', categoryData);
+        } catch (error) {
+          this.$message({ message: 'Anything wrong, please repeat', type: 'error' });
+        }
       }
     },
-    editCategory() {},
   },
 };
 </script>
